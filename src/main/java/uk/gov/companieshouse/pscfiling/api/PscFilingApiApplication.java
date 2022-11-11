df23afc92f086fbbbe431966a7309e56995545f4
PhilipGlover <pglover@companieshouse.gov.uk>
2022-11-11 15:07:20 +0000

    add Spring App class, so service starts and '/persons-with-significant-control/healthcheck' is Up

---
+++ src/main/java/uk/gov/companieshouse/pscfiling/api/PscFilingApiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.pscfiling.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PscFilingApiApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(PscFilingApiApplication.class, args);
+    }
+
+}
