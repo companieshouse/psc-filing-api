5ef8c964cfe147e3b5a26972af803457ffc55f3b
PhilipGlover <pglover@companieshouse.gov.uk>
2023-01-23 13:57:28 +0000

    add validation to match the reference_etag supplied

---
+++ src/test/java/uk/gov/companieshouse/pscfiling/api/validator/PscEtagValidatorTest.java
@@ -0,0 +1,90 @@
+package uk.gov.companieshouse.pscfiling.api.validator;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.validation.FieldError;
+import uk.gov.companieshouse.api.error.ApiErrorResponseException;
+import uk.gov.companieshouse.api.model.psc.PscApi;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
+import uk.gov.companieshouse.pscfiling.api.exception.FilingResourceNotFoundException;
+import uk.gov.companieshouse.pscfiling.api.model.PscTypeConstants;
+import uk.gov.companieshouse.pscfiling.api.model.dto.PscIndividualDto;
+import uk.gov.companieshouse.pscfiling.api.service.PscDetailsService;
+
+@ExtendWith(MockitoExtension.class)
+class PscEtagValidatorTest {
+
+    @Mock
+    private PscDetailsService pscDetailsService;
+    @Mock
+    private PscApi pscApi;
+    @Mock
+    private Transaction transaction;
+    @Mock
+    private PscIndividualDto dto;
+    @Mock
+    private ApiErrorResponseException errorResponseException;
+
+    PscEtagValidator testValidator;
+    private PscTypeConstants pscType;
+    private List<FieldError> errors;
+    private String passthroughHeader;
+
+    private static final String PSC_ID = "1kdaTltWeaP1EB70SSD9SLmiK5Y";
+    private static final String ETAG = "1234567";
+
+    @BeforeEach
+    void setUp() {
+
+        errors = new ArrayList<>();
+        pscType = PscTypeConstants.INDIVIDUAL;
+        passthroughHeader = "passthroughHeader";
+
+        testValidator = new PscEtagValidator(pscDetailsService);
+    }
+
+    @Test
+    void validateWhenEtagMatches() {
+
+        when(dto.getReferencePscId()).thenReturn(PSC_ID);
+        when(dto.getReferenceEtag()).thenReturn(ETAG);
+        when(pscDetailsService.getPscDetails(transaction, PSC_ID, pscType, passthroughHeader )).thenReturn(pscApi);
+        when(pscApi.getEtag()).thenReturn(ETAG);
+
+        testValidator.validate(
+                new FilingValidationContext(dto, errors, transaction, pscType, passthroughHeader));
+
+        assertThat(errors, is(empty()));
+    }
+
+    @Test
+    void validateWhenEtagDoesNotMatch() {
+
+        var fieldError = new FieldError("object", "reference_etag", ETAG, false,
+                new String[]{null, "notMatch.reference_etag"}, null,
+                "Etag for PSC does not match latest value");
+        when(dto.getReferencePscId()).thenReturn(PSC_ID);
+        when(dto.getReferenceEtag()).thenReturn(ETAG);
+        when(pscDetailsService.getPscDetails(transaction, PSC_ID, pscType, passthroughHeader )).thenReturn(pscApi);
+        when(pscApi.getEtag()).thenReturn("some other etag value");
+
+        testValidator.validate(
+                new FilingValidationContext(dto, errors, transaction, pscType, passthroughHeader));
+
+        assertThat(errors.stream().findFirst().orElseThrow(), equalTo(fieldError));
+        assertThat(errors, contains(fieldError));
+    }
+}
+
