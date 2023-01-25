45069c5becca5e4f7f86845e53a3f7c9fb9fd3aa
eedwards0 <eedwards2@companieshouse.gov.uk>
2023-01-25 11:27:04 +0000

    Added parameterised tests with null date checks
    Also updated POM to include parameters for JUnit.
    Now display parameter names in tests
    
    For PSC07 - PSC Individual cessation scenario
    PSC-39

--- src/main/java/uk/gov/companieshouse/pscfiling/api/validator/PscRegisterEntryDateValidator.java
+++ src/main/java/uk/gov/companieshouse/pscfiling/api/validator/PscRegisterEntryDateValidator.java
@@ -9,10 +9,14 @@ public class PscRegisterEntryDateValidator extends BaseFilingValidator implement
     @Override
     public void validate(final FilingValidationContext validationContext) {
 
-        if (validationContext.getDto().getRegisterEntryDate().isBefore(validationContext.getDto().getCeasedOn())) {
+        final var registerEntryDate = validationContext.getDto().getRegisterEntryDate();
+        final var ceasedOnDate = validationContext.getDto().getCeasedOn();
+
+        if (registerEntryDate != null && ceasedOnDate != null && registerEntryDate.isBefore(
+                ceasedOnDate)) {
+
             validationContext.getErrors()
-                    .add(new FieldError("object", "register_entry_date",
-                            validationContext.getDto().getRegisterEntryDate(), false,
+                    .add(new FieldError("object", "register_entry_date", registerEntryDate, false,
                             new String[]{null, "date.register_entry_date"}, null,
                             "PSC register entry date cannot be before the cessation date"));
         }
