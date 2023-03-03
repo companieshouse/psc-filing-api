b2cc9a6ecb6a2c9a5a5575a49f271cc277457814
eedwards0 <eedwards2@companieshouse.gov.uk>
2023-03-03 10:04:42 +0000

    Updating unit tests
    WIP
    
    PSC-71

--- src/test/java/uk/gov/companieshouse/pscfiling/api/controller/FilingDataControllerTest.java
+++ src/test/java/uk/gov/companieshouse/pscfiling/api/controller/FilingDataControllerTest.java
@@ -22,7 +22,7 @@ class FilingDataControllerTest {
         var testController = new FilingDataController(){};
 
         assertThrows(NotImplementedException.class,
-            () -> testController.getFilingsData("trans-id", PscTypeConstants.INDIVIDUAL,
+            () -> testController.getFilingsData("trans-id",
                 "filing-id", transaction, request));
     }
 }
