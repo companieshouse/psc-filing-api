a35d0d53acb39732b8c58b11ef3ba3c6acf0b37b
nhill-ch <nhill@companieshouse.gov.uk>
2024-03-20 11:44:13 +0000

    cleanup

--- src/test/java/uk/gov/companieshouse/pscfiling/api/service/impl/TestBaseService.java
+++ src/test/java/uk/gov/companieshouse/pscfiling/api/service/impl/TestBaseService.java
@@ -16,11 +16,12 @@ public class TestBaseService {
     static final String FILING_ID = "6332aa6ed28ad2333c3a520a";
     static PscTypeConstants mockedValue;
     static MockedStatic<PscTypeConstants> myMockedEnum;
+    static Class<PscTypeConstants> enumPscTypeConstants = PscTypeConstants.class;
 
     //Use to mock out the PscTypeConstants enum class
     @SuppressWarnings("unchecked")
     static <PscTypeConstants extends Enum<PscTypeConstants>> PscTypeConstants[] addNewEnumValue() {
-        final EnumSet<PscTypeConstants> enumSet = EnumSet.allOf((Class<PscTypeConstants>) uk.gov.companieshouse.pscfiling.api.model.PscTypeConstants.class);
+        final EnumSet<PscTypeConstants> enumSet = EnumSet.allOf((Class<PscTypeConstants>) enumPscTypeConstants);
         final PscTypeConstants[] newValues =
                 (PscTypeConstants[]) Array.newInstance(uk.gov.companieshouse.pscfiling.api.model.PscTypeConstants.class, enumSet.size() + 1);
         int i = 0;
@@ -29,7 +30,7 @@ public class TestBaseService {
             i++;
         }
 
-        final PscTypeConstants newEnumValue = mock((Class<PscTypeConstants>) uk.gov.companieshouse.pscfiling.api.model.PscTypeConstants.class);
+        final PscTypeConstants newEnumValue = mock((Class<PscTypeConstants>) enumPscTypeConstants);
         newValues[newValues.length - 1] = newEnumValue;
 
         when(newEnumValue.ordinal()).thenReturn(newValues.length - 1);
